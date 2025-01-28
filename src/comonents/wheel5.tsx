a47b2c708d29b607b9807641fd630601d3e7ffc6
rithikronald <rithikronald29@gmail.com>
2025-01-28 16:09:09 +0530

    Added All functionality

--- src/comonents/wheel5.tsx
+++ src/comonents/wheel5.tsx
@@ -6,7 +6,6 @@ import {
   Image,
   ImageBackground,
   Modal,
-  Pressable,
   StyleSheet,
   Text,
   TouchableOpacity,
@@ -15,20 +14,21 @@ import {
 import LinearGradient from 'react-native-linear-gradient';
 
 const SECTIONS = [
-  {label: '₹100', angle: 0},
-  {label: 'Mini', angle: 45},
-  {label: 'Mega', angle: 90},
-  {label: 'Double', angle: 135},
-  {label: '?', angle: 180},
+  {label: '₹100', angle: 0, uri: require('../assets/rupee.png')},
+  {label: 'Mini', angle: 45, uri: require('../assets/mini.png')},
+  {label: 'Mega', angle: 90, uri: require('../assets/mega.png')},
+  {label: 'Double', angle: 135, uri: require('../assets/double.png')},
+  {label: '?', angle: 180, uri: require('../assets/cashout2.png')},
   {label: 'Cash Out', angle: 225},
-  {label: 'Double', angle: 270},
-  {label: 'Mini', angle: 315},
-  {label: 'Medium', angle: 360},
+  {label: 'Double', angle: 270, uri: require('../assets/double.png')},
+  {label: 'Mini', angle: 315, uri: require('../assets/mini.png')},
+  {label: 'Medium', angle: 360, uri: require('../assets/medium.png')},
 ];
 
 export const Wheel5 = () => {
   const spinValue = useState(new Animated.Value(0))[0];
-  const [isModalOpen, setIsModalOpen] = useState(true);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedPrize, setSelectedPrize] = useState();
 
   const spinWheel = () => {
     const randomSpins = Math.floor(Math.random() * 3) + 3; // Between 3 to 7 spins
@@ -53,6 +53,11 @@ export const Wheel5 = () => {
         'Selected:',
         selectedSection ? selectedSection.label : 'Unknown',
       );
+      console.log(selectedSection);
+      setSelectedPrize(selectedSection);
+      setTimeout(() => {
+        setIsModalOpen(true);
+      }, 1000);
     });
   };
 
@@ -68,7 +73,7 @@ export const Wheel5 = () => {
       />
       <View className="absolute">
         <Animated.Image
-          source={require('../assets/wheel2.png')}
+          source={require('../assets/wheel5.png')}
           style={{
             width: 350,
             height: 350,
@@ -90,7 +95,7 @@ export const Wheel5 = () => {
           />
         </TouchableOpacity>
         <Image
-          className="w-[50px] h-[40px] absolute self-center top-[7%]"
+          className="w-[50px] h-[30px] absolute self-center top-[7%]"
           resizeMode="center"
           source={require('../assets/pointer.png')}
         />
@@ -104,45 +109,54 @@ export const Wheel5 = () => {
           setIsModalOpen(!isModalOpen);
         }}>
         <View className="flex-1 justify-center items-center bg-black/70 p-8">
-          <View className="flex w-full items-end">
-            <TouchableOpacity onPress={() => setIsModalOpen(false)}>
-              <Image
-                className="w-6 h-5"
-                resizeMode="contain"
-                source={require('../assets/x.png')}
-              />
-            </TouchableOpacity>
-          </View>
-          <LinearGradient
-            style={styles.modalView}
-            colors={['#7600EB', '#2B0552']}>
-            <ImageBackground
-              className="w-full h-full flex justify-center items-center p-16"
-              resizeMode="contain"
-              source={require('../assets/backgroundStar.png')}>
-              <Image source={require('../assets/rupee.png')} />
-              <Text className="text-3xl font-bold color-[#FEBD01] my-4">
-                Congratulations!
-              </Text>
-              <Text className="text-white font-bold text-sm text-center mb-8">
-                You have win a ₹100 cash it will be credited on your spin app
-                wallet
-              </Text>
+          {selectedPrize && (
+            <>
+              <View className="flex w-full items-end">
+                <TouchableOpacity onPress={() => setIsModalOpen(false)}>
+                  <Image
+                    className="w-6 h-5"
+                    resizeMode="contain"
+                    source={require('../assets/x.png')}
+                  />
+                </TouchableOpacity>
+              </View>
               <LinearGradient
-                style={{
-                  padding: 2,
-                  borderRadius: 6,
-                  width: 140,
-                  height: 26,
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  marginTop: 10,
-                }}
-                colors={['#FFBA01', '#E08304']}>
-                <Text className="text-white italic font-bold">Cash Out</Text>
+                style={styles.modalView}
+                colors={['#7600EB', '#2B0552']}>
+                <ImageBackground
+                  className="w-full h-full flex justify-center items-center p-16"
+                  resizeMode="contain"
+                  source={require('../assets/backgroundStar.png')}>
+                  <Image
+                    className="w-20 h-16"
+                    resizeMode="center"
+                    source={selectedPrize.uri}
+                  />
+                  <Text className="text-3xl font-bold color-[#FEBD01] my-4">
+                    Congratulations!
+                  </Text>
+                  <Text className="text-white font-bold text-sm text-center mb-8">
+                    {`You have win a ${selectedPrize.label} cash it will be credited on your spin app wallet`}
+                  </Text>
+                  <LinearGradient
+                    style={{
+                      padding: 2,
+                      borderRadius: 6,
+                      width: 140,
+                      height: 26,
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                      marginTop: 10,
+                    }}
+                    colors={['#FFBA01', '#E08304']}>
+                    <Text className="text-white italic font-bold">
+                      Cash Out
+                    </Text>
+                  </LinearGradient>
+                </ImageBackground>
               </LinearGradient>
-            </ImageBackground>
-          </LinearGradient>
+            </>
+          )}
         </View>
       </Modal>
     </View>
